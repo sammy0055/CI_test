79f5cd8f3f5f25dcd98df261256538fc71c55a6d
sammy0055 <ronicksamuel@gmail.com>
2023-01-26 14:20:19 +0100

    movetest

--- tests/sum.test.ts
+++
@@ -1,8 +0,0 @@
-import { app } from "../src/app";
-import request from "supertest";
-describe("testing for sum functon", () => {
-  it("should return two", async () => {
-    const res = await request(app).get("/");
-    expect(res.statusCode).toBe(200);
-  });
-});
