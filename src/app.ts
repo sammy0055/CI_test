ef2e3ddaf6bf18c4e2076ce7431f49dbee1affbb
Samuel Ronick <ronicksamuel@gmail.com>
2023-04-27 19:29:51 +0100

    ignore

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server new");
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+// });
 
