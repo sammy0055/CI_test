c762dda7def4e94c5f9dd42613d02e9336731bb6
Samuel Ronick <ronicksamuel@gmail.com>
2023-04-27 19:24:32 +0100

    changed response message

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,10 @@ export const app: Express = express();
 const port = 5000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  res.send("Express + TypeScript Server new");
 });
 
-// app.listen(port, () => {
-//   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-// });
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
 
